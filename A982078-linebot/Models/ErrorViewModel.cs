e7c694bd2d6d7c70f2cd63ae83f37d679c938b9b
A982078 <cindy82320888@gmail.com>
2023-05-15 10:09:43 +0800

    init

---
+++ A982078-linebot/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace A982078_linebot.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
