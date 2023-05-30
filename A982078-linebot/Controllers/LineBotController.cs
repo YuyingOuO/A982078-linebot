b35e7b3eb33ccb59a9a515567e49568702ac01de
YuyingMeow <cindy82320888@gmail.com>
2023-05-30 17:21:34 +0800

    ngrok還沒上

---
+++ A982078-linebot/Controllers/LineBotController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Line.Messaging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace A982078_linebot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LineBotController : ControllerBase
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly HttpContext _httpContext;
+        private readonly LineBotConfig _lineBotConfig;
+
+        public LineBotController(IServiceProvider serviceProvider)
+        {
+            _httpContextAccessor = serviceProvider.GetRequiredService<IHttpContextAccessor>();
+            _httpContext = _httpContextAccessor.HttpContext;
+            _lineBotConfig = new LineBotConfig();
+            _lineBotConfig.channelSecret = "9485f1110e17041495e11645217067c4";
+            _lineBotConfig.accessToken = "QYFHrfpUb2U4xR4y2b48k90GIsYvb8uIxy6gW2MXXUsXHWUA/NeTMXIwRM7lj/XbrjgZxuwjLXHc60TLPXA8tXlsNUCNsGucIsALaMCzHRwW9S6Jxy5ocAy4GTrP9mA7L4W5qXoW/SKPwezXOFq/KAdB04t89/1O/w1cDnyilFU=";
+        }
+        
+        //完整的路由網址就是 https://xxx/api/linebot/run
+        [HttpPost("run")]
+        public async Task<IActionResult> Post()
+        {
+            try
+            {
+                var events = await _httpContext.Request.GetWebhookEventsAsync(_lineBotConfig.channelSecret);
+                var lineMessagingClient = new LineMessagingClient(_lineBotConfig.accessToken);
+                var lineBotApp = new LineBotApp(lineMessagingClient);
+                await lineBotApp.RunAsync(events);
+            }
+            catch (Exception ex)
+            {
+                //需要 Log 可自行加入
+                //_logger.LogError(JsonConvert.SerializeObject(ex));
+            }
+            return Ok();
+        }
+    }
+}
