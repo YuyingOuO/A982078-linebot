9544a9c31a80ff78b203396761c423785625e626
user <Cindy82320888@gmail.com>
2023-06-09 16:02:53 +0800

    新增喵喵

--- A982078-linebot/Controllers/LineBotController.cs
+++ A982078-linebot/Controllers/LineBotController.cs
@@ -12,17 +12,16 @@ namespace A982078_linebot.Controllers
     [ApiController]
     public class LineBotController : ControllerBase
     {
-        private readonly IHttpContextAccessor _httpContextAccessor;
-        private readonly HttpContext _httpContext;
+        private readonly HttpContext? _httpContext;
         private readonly LineBotConfig _lineBotConfig;
 
         public LineBotController(IServiceProvider serviceProvider)
         {
-            _httpContextAccessor = serviceProvider.GetRequiredService<IHttpContextAccessor>();
-            _httpContext = _httpContextAccessor.HttpContext;
+            var httpContextAccessor = serviceProvider.GetRequiredService<IHttpContextAccessor>();
+            _httpContext = httpContextAccessor.HttpContext;
             _lineBotConfig = new LineBotConfig();
             _lineBotConfig.channelSecret = "9485f1110e17041495e11645217067c4";
-            _lineBotConfig.accessToken = "QYFHrfpUb2U4xR4y2b48k90GIsYvb8uIxy6gW2MXXUsXHWUA/NeTMXIwRM7lj/XbrjgZxuwjLXHc60TLPXA8tXlsNUCNsGucIsALaMCzHRwW9S6Jxy5ocAy4GTrP9mA7L4W5qXoW/SKPwezXOFq/KAdB04t89/1O/w1cDnyilFU=";
+            _lineBotConfig.accessToken = "mkR1uUX9AEiAGJzCnHwwgyNhUhLZB0J3fTOApG3wBHLIxniiQw8UWej+f20XAzbnrjgZxuwjLXHc60TLPXA8tXlsNUCNsGucIsALaMCzHRxo/UP589G3ikXik2fttsA16Zwyp6FPDaJY8nd8g4svCwdB04t89/1O/w1cDnyilFU=";
         }
         
         //完整的路由網址就是 https://xxx/api/linebot/run
@@ -31,12 +30,15 @@ namespace A982078_linebot.Controllers
         {
             try
             {
-                var events = await _httpContext.Request.GetWebhookEventsAsync(_lineBotConfig.channelSecret);
-                var lineMessagingClient = new LineMessagingClient(_lineBotConfig.accessToken);
-                var lineBotApp = new LineBotApp(lineMessagingClient);
-                await lineBotApp.RunAsync(events);
+                if (_httpContext != null)
+                {
+                    var events = await _httpContext.Request.GetWebhookEventsAsync(_lineBotConfig.channelSecret);
+                    var lineMessagingClient = new LineMessagingClient(_lineBotConfig.accessToken);
+                    var lineBotApp = new LineBotApp(lineMessagingClient);
+                    await lineBotApp.RunAsync(events);
+                }
             }
-            catch (Exception ex)
+            catch (Exception exp)
             {
                 //需要 Log 可自行加入
                 //_logger.LogError(JsonConvert.SerializeObject(ex));
